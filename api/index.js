f38d7c1a5d76eb12385eabb468098f2b274064ca
rohitkumar305 <rohitkumar352007@gmail.com>
2026-06-01 16:09:55 +0530

    Fix duplicate API routes by removing compiled JS files

--- api/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = handler;
-function handler(req, res) {
-    res.status(200).json({ message: 'Dential Clinic API is running.' });
-}
